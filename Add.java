6d4e8ade3b45ca3eaafafd258b553cda4004fbd7
LikhithaYada <123145389+LikhithaYada@users.noreply.github.com>
2023-06-17 08:25:52 -0700

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,16 @@
+package programs;
+import java.util.*;
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("enter a and b values?");
+     Scanner obj=new Scanner(System.in);
+     int a=obj.nextInt();
+     int b=obj.nextInt();
+     int 
+     c=a+b;
+     System.out.println("addition is :- "+c);
+	}
+
+}
